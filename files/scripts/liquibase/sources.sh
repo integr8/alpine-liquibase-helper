fcc1054272570d402f997462d723afcff4956021
Fábio Luciano <fabio@naoimporta.com>
2018-12-16 12:51:16 -0200

    Testando o build pelo travis

--- files/scripts/liquibase/sources.sh
+++ files/scripts/liquibase/sources.sh
@@ -4,7 +4,7 @@ if [ "$SOURCE_METHOD" == 'GIT' ]; then
     : ${GIT_URL?   "Por favor, informe o endereço do repositório do git" }
     : ${GIT_USER?  "Por favor, informe o usuário do git" }
     : ${GIT_EMAIL? "Por favor, informe o email do usuário git" }
-    : ${GIT_PASS?  "Por favor, informe a senha do usupario do git" }
+    : ${GIT_PASS?  "Por favor, informe a senha do usuario do git" }
 
     git config --global credential.username ${GIT_USER}
     git config --global credential.helper '!f() { echo "password='${GIT_PASS}'"; }; f'
@@ -18,5 +18,4 @@ elif [ "$SOURCE_METHOD" == 'VOLUME' ]; then
     if [ ! -d "$SOURCE_PATH" ]; then
         echo 'Foi informado que o código fonte seria provido por um volume, mas o diretório não existe'
     fi
-
 fi
